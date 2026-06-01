43e3ec996ffc6a5a0e2ccfeff6c2ba94f76fd89b
meenakshikandwal194-del <meenakshi.kandwal194@gmail.com>
2026-06-01 01:47:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify, render_template
-from google import genai
+import requests
 import os
 
 app = Flask(__name__)
 
-client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
 SYSTEM_PROMPT = """
 You are Azure Dost AI.
@@ -20,14 +20,25 @@ def home():
 def chat():
     user_message = request.json["message"]
 
-    prompt = SYSTEM_PROMPT + "\nUser question: " + user_message
+    full_prompt = SYSTEM_PROMPT + "\nUser question: " + user_message
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=prompt
+    response = requests.post(
+        "https://api.groq.com/openai/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {GROQ_API_KEY}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "model": "llama3-8b-8192",
+            "messages": [
+                {"role": "user", "content": full_prompt}
+            ]
+        }
     )
 
-    return jsonify({"reply": response.text})
+    ai_reply = response.json()["choices"][0]["message"]["content"]
+
+    return jsonify({"reply": ai_reply})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

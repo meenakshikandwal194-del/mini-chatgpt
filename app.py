e1b57c5b51bf34d3f227f1dea0233c1aa3368589
meenakshikandwal194-del <meenakshi.kandwal194@gmail.com>
2026-06-01 01:59:34 +0100

    added groq ai

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, request, jsonify, render_template
-import requests
+from groq import Groq
 import os
 
 app = Flask(__name__)
 
-GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
-
-SYSTEM_PROMPT = """
-You are Azure Dost AI.
-Explain Azure and cloud concepts to non-IT beginners in simple Hindi-English.
-Use real-life examples and beginner-friendly language.
-"""
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY")
+)
 
 @app.route("/")
 def home():
@@ -18,27 +14,24 @@ def home():
 
 @app.route("/chat", methods=["POST"])
 def chat():
+
     user_message = request.json["message"]
 
-    full_prompt = SYSTEM_PROMPT + "\nUser question: " + user_message
-
-    response = requests.post(
-        "https://api.groq.com/openai/v1/chat/completions",
-        headers={
-            "Authorization": f"Bearer {GROQ_API_KEY}",
-            "Content-Type": "application/json"
-        },
-        json={
-            "model": "llama3-8b-8192",
-            "messages": [
-                {"role": "user", "content": full_prompt}
-            ]
-        }
+    completion = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=[
+            {
+                "role": "user",
+                "content": user_message
+            }
+        ]
     )
 
-    ai_reply = response.json()["choices"][0]["message"]["content"]
+    ai_reply = completion.choices[0].message.content
 
-    return jsonify({"reply": ai_reply})
+    return jsonify({
+        "reply": ai_reply
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
